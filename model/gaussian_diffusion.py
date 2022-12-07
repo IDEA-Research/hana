7ff4388a41f2f93162b4ae7c72d1b78017a8a0a4
ntianhe ren <rentianhe@dgx061.scc.idea>
2022-12-07 16:11:54 +0800

    add LICENSE

--- model/gaussian_diffusion.py
+++ model/gaussian_diffusion.py
@@ -1,9 +1,21 @@
-
-"""
-This code started out as a PyTorch port of Ho et al's diffusion models:
-https://github.com/hojonathanho/diffusion/blob/1e0dceb3b3495bbe19116a5e1b3596cd0706c543/diffusion_tf/diffusion_utils_2.py
-Docstrings have been added, as well as DDIM sampling and a new collection of beta schedules.
-"""
+# coding=utf-8
+# Copyright 2022 The IDEA Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------------------------
+# Modified from:
+# 
+# ------------------------------------------------------------------------------------------------
 
 import enum
 import math
