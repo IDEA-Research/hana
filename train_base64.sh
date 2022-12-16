fd847b2aec73537509a46e53ddbb24e77a30dc55
CiaoHe <hcaoaf@connect.ust.hk>
2022-12-16 18:12:15 +0800

    update main readme

--- train_base64.sh
+++ train_base64.sh
@@ -2,14 +2,15 @@
 NODES=1
 GPUS_PER_NODE=2
 MINI_BATCH_SIZE=16
+VAL_BATCH_SIZE=4
+MAPPING_FILE='path/to/mapping_file'
+CONFIG_PATH='path/to/config_path'
 
-srun -N $NODES --gres gpu:$GPUS_PER_NODE --ntasks-per-node=$GPUS_PER_NODE \
-    --qos ai4cvr-1 --cpus-per-task 30 \
-    python -W ignore train_base64.py \
-        --mapping_file /comp_robot/mm_generative/data/cc3m/cc3m_map.json \
-        --config_path config/xiaobai_tiny64.yaml \
-        --train_micro_batch_size_per_gpu $MINI_BATCH_SIZE \
-        --val_batch_size 4 \
-        --gpus $GPUS_PER_NODE \
-        --num_nodes $NODES \
-        --fp16 --wandb_debug
+python -W ignore train_base64.py \
+    --mapping_file $MAPPING_FILE \
+    --config_path $CONFIG_PATH \
+    --train_micro_batch_size_per_gpu $MINI_BATCH_SIZE \
+    --val_batch_size $VAL_BATCH_SIZE \
+    --gpus $GPUS_PER_NODE \
+    --num_nodes $NODES \
+    --fp16 --wandb_debug
